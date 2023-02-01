05e6b90e14873fb25e36a7acf6143dcba0f87fa7
Ziru Zou <jiyucoding@yahoo.com>
2023-02-01 09:50:45 -0500

    set up isInCart state variable, change li className and btn textContent when click

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false)
+  const itemClass = isInCart ? "in-cart" : ""
+  const btnContent = isInCart ? "Remove From Cart" : "Add to Cart"
+
+  function handleAddItem() {
+    setIsInCart(!isInCart)
+  }
+  
+
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddItem}>{btnContent}</button>
     </li>
   );
 }
